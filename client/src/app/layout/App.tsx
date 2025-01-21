fb378e86d74a954e19f2ef5718b6d12465ea23e4
Cell6969 <bossmarinoo@gmail.com>
2025-01-21 13:51:14 +0700

    add: header

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { Product } from "../models/product";
 import Catalog from "../../features/catalog/Catalog";
-import { Box, Button, Container, Typography } from "@mui/material";
+import { Container } from "@mui/material";
+import Navbar from "./Navbar";
 
 function App() {
   const [product, setProduct] = useState<Product[]>([]);
@@ -12,32 +13,13 @@ function App() {
       .then((data) => setProduct(data));
   }, []);
 
-  const addProduct = () => {
-    setProduct((prevState) => [
-      ...prevState,
-      {
-        id: prevState.length + 1,
-        name: "product" + (prevState.length + 1),
-        price: prevState.length * 100 + 100,
-        stock: 100,
-        description: "test",
-        url: "https://image/1",
-        type: "test",
-        brand: "test",
-      },
-    ]);
-  };
-
   return (
-    <Container maxWidth="xl">
-      <Box display="flex" justifyContent="center" gap={3} marginY={3}>
-        <Typography variant="h4">E-Commerce</Typography>
-        <Button variant="contained" onClick={addProduct}>
-          Add Product
-        </Button>
-      </Box>
-      <Catalog product={product} />
-    </Container>
+    <>
+      <Navbar />
+      <Container maxWidth="xl" sx={{ mt: 14 }}>
+        <Catalog product={product} />
+      </Container>
+    </>
   );
 }
 
---
+++ client/src/app/layout/Navbar.tsx
@@ -0,0 +1,13 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+
+export default function Navbar() {
+  return (
+    <AppBar position="fixed">
+        <Toolbar>
+            <Typography variant="h6">
+                E-Com
+            </Typography>
+        </Toolbar>
+    </AppBar>
+  )
+}
