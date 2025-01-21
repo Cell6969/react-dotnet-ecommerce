f83216d94aa5053833f60b34ee0c614d9e59f4c4
Cell6969 <bossmarinoo@gmail.com>
2025-01-21 11:51:01 +0700

    update: config for config client cors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div style={{ fontSize: "1.6rem" }}>E-Commerce</div>;
+  const [product, setProduct] = useState<{ name: string; price: number }[]>([]);
+
+  useEffect(() => {
+    fetch("https://localhost:5227/api/products")
+      .then((response) => response.json())
+      .then((data) => setProduct(data));
+  }, []);
+
+  return (
+    <div>
+      <h1 style={{ color: "red" }}>E-Commerce</h1>
+      <ul>
+        {product.map((item, index) => (
+          <li key={index}>
+            {item.name} - {item.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
