4c855255abe60481442a95f838dd45270b155981
Cell6969 <bossmarinoo@gmail.com>
2025-02-14 14:14:04 +0700

    update: style for cart

--- client/src/features/cart/CartPage.tsx
+++ client/src/features/cart/CartPage.tsx
@@ -1,16 +1,21 @@
-import { Typography } from "@mui/material";
+import { Grid2, Typography } from "@mui/material";
 import { useGetCartQuery } from "../../api/cartApi";
+import CartItem from "./CartItem";
 
 export default function CartPage() {
   const { isLoading, data } = useGetCartQuery();
 
-  if (isLoading) return <Typography>Loading ....</Typography>
+  if (isLoading) return <Typography>Loading ....</Typography>;
 
-  if (!data) return <Typography variant="h3">Your cart is empty</Typography>
+  if (!data) return <Typography variant="h3">Your cart is empty</Typography>;
 
   return (
-    <div>
-      {data.cartId}
-    </div>
+    <Grid2 container spacing={2}>
+      <Grid2 size={8}>
+        {data.items.map((item) => (
+          <CartItem item={item} key={item.productId} />
+        ))}
+      </Grid2>
+    </Grid2>
   );
 }
